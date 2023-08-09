7976c8ba00987703a6352f70f87b2912b658d75d
tsg-ritmmbl <131126661+tsg-ritmmbl@users.noreply.github.com>
2023-08-09 04:50:25 +0000

    added home page

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def mbl_digiform():
-  return "MBL/ATML BORROWER'S DIGIFORM!"
+  return render_template('home.html')
+
+  #"MBL/ATML BORROWER'S DIGIFORM!"
 
 
 app.run(host='0.0.0.0', port=8080)
